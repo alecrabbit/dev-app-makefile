04346714493c309e91d68a4dc72ae3648e0add2e
Alec Rabbit <alec.therabbit.git@gmail.com>
2023-03-07 17:23:43 +0200

    ~wp

--- .tools/.dam/.sh/helper/common/options.sh
+++ .tools/.dam/.sh/helper/common/options.sh
@@ -52,7 +52,7 @@ common_read_option () {
                     console_fatal "Option is not applicable"
                 fi
                 debug_option "${__OPTION}" "${__VALUE}"
-                _pts_upgrade_run "${__VALUE}"
+                console_debug "$(_pts_upgrade_run "${__VALUE}")"
                 exit
                 ;;
             -V | --version)
