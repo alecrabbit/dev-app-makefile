493b44e3460ec0ca30b75c9ee089dfc2e51ae68f
Alec Rabbit <alec.therabbit.git@gmail.com>
2023-03-08 13:24:15 +0200

    ~wp

--- .tools/.dam/.sh/helper/common/functions.sh
+++ .tools/.dam/.sh/helper/common/functions.sh
@@ -3,6 +3,8 @@
 SCRIPT_START_TIME=$(date +%s)
 export SCRIPT_START_TIME
 
+__DAM_DIR=".tools/.dam"
+
 func_check_user () {
     console_debug "Checking user: $(whoami)"
     if user_is_root
@@ -129,8 +131,8 @@ __updater_install () {
         console_debug "Writing new build ${__build} to file ${BUILD_FILE}$(echo "${__build}" > "${BUILD_FILE}" 2>&1)"
         console_comment "Installing package"
         console_debug "Deleting dev module '${PTS_AUX_DEV_MODULE}'\n$(rm -v "${__dir}/${__package}-${__version}/${PTS_AUX_DEV_MODULE}" 2>&1)"
-        __old_version_dir="${__dest_dir}/.tools/.dam"
-        console_debug "Deleting old version dir '${__old_version_dir}'\n$(rm -vfr "${__old_version_dir}" 2>&1)"
+        __old_dir="${__dest_dir}/${__DAM_DIR}"
+        console_debug "Deleting old version dir '${__old_dir}'\n$(rm -vfr "${__old_dir}" 2>&1)"
         console_debug "Copying new files to '${__dest_dir}'\n$(cp -rv "${__dir}/${__package}-${__version}"/. "${__dest_dir}"/. 2>&1)"
         # shellcheck disable=SC2116
         console_debug "Writing new version ${__version} to file ${VERSION_FILE}$(echo "${__version}" > "${VERSION_FILE}" 2>&1)"
